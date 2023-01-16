2223038139b5ea7015089aca3a803d483f93088f
teodorcvijovic <cvijovicteodor@gmail.com>
2023-01-16 01:49:28 +0100

    add compiler class

---
+++ MJCompiler/src/rs/ac/bg/etf/pp1/Compiler.java
@@ -0,0 +1,69 @@
+package rs.ac.bg.etf.pp1;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
+
+import org.apache.log4j.Logger;
+
+import java_cup.runtime.Symbol;
+import rs.ac.bg.etf.pp1.ast.Program;
+import rs.etf.pp1.mj.runtime.Code;
+import rs.etf.pp1.symboltable.Tab;
+
+public class Compiler {
+	
+	public static void main(String[] args) throws Exception {
+		
+		Logger log = Logger.getLogger(Compiler.class);
+				
+		Reader br = null;
+		try {
+			File sourceCode = new File(args[0]);
+			log.info("Compiling source file: " + sourceCode.getAbsolutePath());
+			
+			br = new BufferedReader(new FileReader(sourceCode));
+			Yylex lexer = new Yylex(br);
+			
+			MJParser p = new MJParser(lexer);
+	        Symbol s = p.parse();  //pocetak parsiranja
+	        
+	        Program prog = (Program)(s.value); 
+	        TabExtension.init();
+	        
+			// ispis sintaksnog stabla
+			log.info(prog.toString(""));
+			log.info("===================================");
+
+			// ispis prepoznatih programskih konstrukcija
+			SemanticAnalyzer v = new SemanticAnalyzer();
+			prog.traverseBottomUp(v); 
+
+			log.info("===================================");
+			Tab.dump();
+			
+			if(!p.errorDetected && v.passed()){
+				File objFile = new File("test/program.obj");
+				if(objFile.exists()) objFile.delete();
+				
+				CodeGenerator codeGenerator = new CodeGenerator();
+				prog.traverseBottomUp(codeGenerator);
+				Code.dataSize = SemanticAnalyzer.programVarCount;
+				Code.mainPc = codeGenerator.getMainPC();
+				Code.write(new FileOutputStream(objFile));
+				log.info("Parsiranje uspesno zavrseno!");
+			}else{
+				log.error("Parsiranje NIJE uspesno zavrseno!");
+			}
+			
+		} 
+		finally {
+			if (br != null) try { br.close(); } catch (IOException e1) { log.error(e1.getMessage(), e1); }
+		}
+		
+	}
+
+}
